8497334185aabaf74dbb63c45220e9762101254f
zhy19950705 <1144567324@qq.com>
2018-03-14 15:16:04 +0800

    'first'

---
+++ src/views/grid/index.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <div class="grid-container">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <!--<div>7</div>-->
+    </div>
+
+    <div class="grid-container2">
+      <article>xhh</article>
+      <div class="header">Header</div>
+      <section>zz</section>
+      <div class="left">Left</div>
+      <div class="main">Main</div>
+      <div class="right">Right</div>
+      <article>xhh</article>
+      <div class="footer">Footer</div>
+      <section>zz</section>
+    </div>
+
+    <div class="grid-container3">
+      <div class="item-a">1</div>
+      <div class="item-b">2</div>
+      <div class="item-c">3</div>
+      <div class="item-d">4</div>
+    </div>
+    <p>autofit</p>
+    <div class="grid-container4">
+      <div v-for="i of 9">
+        <div>{{i}}</div>
+      </div>
+    </div>
+    <p>autofill</p>
+    <div class="grid-container5">
+      <div v-for="i of 9">
+        <div>{{i}}</div>
+      </div>
+    </div>
+
+    <div class="parent">
+      <div class="child">DEMO</div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+    export default {
+      name: 'grid'
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    $grid-gap:2px 4px;
+    .grid-container{
+      grid-template-columns: repeat(3,1fr);
+      grid-template-rows: repeat(2,1fr);
+    }
+    .grid-container2{
+      height: 500px;
+      grid-template-columns: 100px 1fr 100px;
+      grid-template-rows: 100px 1fr 100px;
+      border: 1px solid green{
+        top: 2px;
+        bottom: 2px;
+      }
+      .left{
+        grid-column: 1/2;
+      }
+      .main{
+        grid-column: 2/-2;
+      }
+      .right{
+        grid-column:-2/-2;
+      }
+      .header{
+        grid-column: 2/-2;
+      }
+      .footer{
+        grid-column: 3/2;
+      }
+    }
+    .grid-container3{
+      grid-template-columns: repeat(4,1fr);
+      grid-template-rows: auto;
+      grid-template-areas: "header header header header"
+      "main main . sidebar"
+      "footer footer footer footer";
+      .item-a{
+        grid-area: header;
+      }
+      .item-b{
+        grid-area:main
+      }
+      .item-c{
+        grid-area: sidebar;
+      }
+      .item-d{
+        grid-area: footer;
+      }
+    }
+    .grid-container4{
+      grid-template-rows: 100px;
+      grid-template-columns: repeat(auto-fit,minmax(25px,1fr));
+      div{
+        height: 100%;
+      }
+      /*div>div{*/
+        /*background: yellow;*/
+      /*}*/
+    }
+    .grid-container5{
+      grid-template-rows: 100px;
+      grid-template-columns: repeat(auto-fill,minmax(25px,1fr));
+    }
+
+  @mixin grid-div{
+    display: grid;
+    margin-bottom: 10px;
+    grid-gap: $grid-gap;
+    div{
+      font-size:35px;
+      font-weight:bold;
+      color:#fff;
+      text-align: center;
+      background: #666666;
+      &:nth-child(2n){
+        background: #336666;
+      }
+      &:nth-child(4n){
+        background: #2bff50;
+      }
+      &:hover {background: #ff1b0c
+      }
+    }
+  }
+    .grid-container,.grid-container2,.grid-container3,.grid-container4,.grid-container5{
+        @include grid-div
+      }
+
+</style>
---
+++ src/views/settimeout/settimeout.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <h1>setTimeout</h1>
+    <span id="content">时间</span>
+    <button @click="run()">开始</button>
+    <button @click="stop()">停止</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    // created() {
+    //   if(this)
+    // }
+    data() {
+      return {
+        x: 0,
+        y: 0,
+        z: 0,
+        xh: 'wxhn',
+        obj: {
+          xh: 'zz',
+          hh: function () {
+            console.log(this.xh);
+          }
+        }
+      };
+    },
+    watch: {
+      x() {
+        // setTimeout("this.start", 1000)
+      }
+    },
+    created() {
+      // setTimeout((a, b) => {
+      //   console.log(0 + a + b);
+      // }, 1000, 2, 3);
+      // debugger
+      for (var i = 1; i <= 5; i++) {
+        setTimeout(function timer() {
+          console.log(i);
+        }, i * 1000);
+      }
+    },
+    beforeRouteLeave(to, from, next) {
+      clearInterval(this.invId);
+      next();
+    },
+    methods: {
+      start() {
+        // debugger
+        if (this.x <= 59 && this.x >= 0 && this.y <= 59 && this.y >= 0 && this.z <= 59 && this.z >= 0) {
+          const content = document.getElementById('content');
+          content.innerHTML = `${this.z}:${this.y}:${this.x}`;
+          console.log(this.x);
+          this.x = this.x + 1;
+        } else if (this.y <= 59 && this.y >= 0 && this.z <= 59 && this.z >= 0) {
+          this.y = this.y + 1;
+          this.x = 0;
+        } else if (this.z <= 59 && this.z >= 0) {
+          this.z = this.z + 1;
+          this.x = 0;
+          this.y = 0;
+        }
+      },
+      run() {
+        this.invId = setInterval(() => {
+          this.start();
+        }, 1000);
+      },
+      stop() {
+        clearInterval(this.invId);
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
